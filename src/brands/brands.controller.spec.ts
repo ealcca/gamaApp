53b97328d843ed71fcc05bd5327beba9cd73f1e6
ealcca <edual986@gmail.com>
2024-10-24 17:14:23 -0300

    elimino comentarios x el lint

--- src/brands/brands.controller.spec.ts
+++ src/brands/brands.controller.spec.ts
@@ -43,45 +43,4 @@ describe('BrandsController', () => {
             id: expect.any(Number),
         });
     });
-  
-  });
-  
-/* con este no tenia problemas pero entiendo que es por tratarse de un mock
-
-import {Test,TestingModule} from '@nestjs/testing';
-import { BrandsController } from './brands.controller';
-import { BrandsService } from './brands.service';
-import { Brand } from './entities/brand.entity';
-
-describe('BrandsController',() => {
-    let controller:BrandsController;
-    let service:BrandsService;
-
-    beforeAll(async () => {
-        const module: TestingModule = await Test.createTestingModule({
-            controllers: [BrandsController],
-            providers:[
-                {
-                    provide: BrandsService,
-                    useValue: {
-                        findAll: jest.fn().mockResolvedValue([
-                            {id:1, trademark:'tesla'},
-                            {id:2, trademark:'ford'},
-                        ]),
-                    },
-                },
-            ],
-        }).compile();
-
-        controller = module.get<BrandsController>(BrandsController);
-        service = module.get<BrandsService>(BrandsService);
-    });
-
-    it('should return an array of brans', async () => {
-        const result = await controller.findAll();
-        expect(result).toBeInstanceOf(Array);
-        expect(result).toHaveLength(2);
-        expect(result[0]).toHaveProperty('trademark','tesla');
-    });
 });
-*/
