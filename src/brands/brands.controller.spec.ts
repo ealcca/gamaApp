e95bcf775097d8e0ce01b99003cd5f3dd49f0672
ealcca <edual986@gmail.com>
2024-10-24 17:02:23 -0300

    implementado test de integracion en brand

--- src/brands/brands.controller.spec.ts
+++ src/brands/brands.controller.spec.ts
@@ -2,6 +2,56 @@ import {Test,TestingModule} from '@nestjs/testing';
 import { BrandsController } from './brands.controller';
 import { BrandsService } from './brands.service';
 import { Brand } from './entities/brand.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+import { Car } from '../cars/entities/car.entity';
+dotenv.config();
+
+describe('BrandsController', () => {
+    let controller: BrandsController;
+  
+    beforeEach(async () => {
+      const module: TestingModule = await Test.createTestingModule({
+        controllers: [BrandsController],
+        providers: [BrandsService],
+        imports: [
+          TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.DB_HOST,
+            port: parseInt(process.env.DB_PORT),
+            username: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: `${process.env.DB_NAME}_test`, 
+            entities: [Car, Brand],
+            synchronize: true,
+          }),
+          TypeOrmModule.forFeature([Brand]),
+        ],
+      }).compile();
+  
+      controller = module.get<BrandsController>(BrandsController);
+    });
+  
+     
+    it('should create a brand and return it with its id', async () => {
+        expect(
+            await controller.create({
+                trademark: 'tesla',
+            }),
+        ).toEqual({
+            trademark: 'tesla',
+            id: expect.any(Number),
+        });
+    });
+  
+  });
+  
+/* con este no tenia problemas pero entiendo que es por tratarse de un mock
+
+import {Test,TestingModule} from '@nestjs/testing';
+import { BrandsController } from './brands.controller';
+import { BrandsService } from './brands.service';
+import { Brand } from './entities/brand.entity';
 
 describe('BrandsController',() => {
     let controller:BrandsController;
@@ -34,3 +84,4 @@ describe('BrandsController',() => {
         expect(result[0]).toHaveProperty('trademark','tesla');
     });
 });
+*/
