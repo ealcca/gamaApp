10e970e740b46375e09fa3bcccdbd4c748f984ce
ealcca <edual986@gmail.com>
2024-10-24 21:23:44 -0300

    Test de integracion (#6)
    Correcciones en el test de integracion

--- src/brands/brands.controller.spec.ts
+++ src/brands/brands.controller.spec.ts
@@ -2,35 +2,45 @@ import {Test,TestingModule} from '@nestjs/testing';
 import { BrandsController } from './brands.controller';
 import { BrandsService } from './brands.service';
 import { Brand } from './entities/brand.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+import { Car } from '../cars/entities/car.entity';
+dotenv.config();
 
-describe('BrandsController',() => {
-    let controller:BrandsController;
-    let service:BrandsService;
-
-    beforeAll(async () => {
-        const module: TestingModule = await Test.createTestingModule({
-            controllers: [BrandsController],
-            providers:[
-                {
-                    provide: BrandsService,
-                    useValue: {
-                        findAll: jest.fn().mockResolvedValue([
-                            {id:1, trademark:'tesla'},
-                            {id:2, trademark:'ford'},
-                        ]),
-                    },
-                },
-            ],
-        }).compile();
-
-        controller = module.get<BrandsController>(BrandsController);
-        service = module.get<BrandsService>(BrandsService);
+describe('BrandsController', () => {
+    let controller: BrandsController;
+  
+    beforeEach(async () => {
+      const module: TestingModule = await Test.createTestingModule({
+        controllers: [BrandsController],
+        providers: [BrandsService],
+        imports: [
+          TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.DATABASE_HOST,
+            port: parseInt(process.env.DATABASE_PORT),
+            username: process.env.DATABASE_USER,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE_NAME, 
+            entities: [Car, Brand],
+            synchronize: true,
+          }),
+          TypeOrmModule.forFeature([Brand]),
+        ],
+      }).compile();
+  
+      controller = module.get<BrandsController>(BrandsController);
     });
-
-    it('should return an array of brans', async () => {
-        const result = await controller.findAll();
-        expect(result).toBeInstanceOf(Array);
-        expect(result).toHaveLength(2);
-        expect(result[0]).toHaveProperty('trademark','tesla');
+  
+     
+    it('should create a brand and return it with its id', async () => {
+        expect(
+            await controller.create({
+                trademark: 'tesla_TeST',
+            }),
+        ).toEqual({
+            trademark: 'tesla_TeST',
+            id: expect.any(Number),
+        });
     });
 });
